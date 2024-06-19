b1f43175b5a34d50bcb0f7e545202d6667b68ed9
Kamil Skupień <kil85@interia.pl>
2024-06-19 22:44:48 +0200

    Contact section finished and slide nav problem solved

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const year = document.querySelector(".footer__year");
 const burgerBtn = document.querySelector(".hamburger");
 const mobileNav = document.querySelector(".nav-mobile");
+const navBtns = document.querySelectorAll(".nav__link");
 
 const handleYear = () => {
 	const currentYear = new Date().getFullYear();
@@ -11,6 +12,12 @@ const handleYear = () => {
 const handleNav = () => {
 	burgerBtn.classList.toggle("is-active");
 	mobileNav.classList.toggle("nav-mobile--active");
+	navBtns.forEach((i) => {
+		i.addEventListener("click", () => {
+			burgerBtn.classList.remove("is-active");
+			mobileNav.classList.remove("nav-mobile--active");
+		});
+	});
 };
 
 handleYear();
